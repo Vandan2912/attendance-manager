83234078b96b286e2991391b8e715cb80b895da5
vandan <vandanpatel29122001@gmail.com>
2025-03-06 01:06:59 +0530

    config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // dont check for eslink rules
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
